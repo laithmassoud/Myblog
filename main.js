48f3002fba1666b4c401bae184305995af6d71fb
laithmassoud <laythmassoud8@gmail.com>
2017-11-16 15:22:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -46,21 +46,15 @@ $(document).ready(function() {
     if (event.keyCode === 39) {
       if (parseInt(main.attr("class").slice(3, 4)) < $('#navi ul').children().length) {
         main.hide();
-        list.removeAttr('id');
         main.next().fadeIn();
-        list.next().attr('id','aboutme');
         main = main.next();
-        list = list.next();
       }
     }
     if (event.keyCode === 37) {
       if (parseInt(mainClass.attr("class").slice(3, 4)) > 1) {
         main.hide();
-        list.removeAttr('id');
         main.prev().fadeIn();
-        list.prev().attr('id','aboutme');
         main = main.prev();
-        list = list.prev();
       }
     }
   });
